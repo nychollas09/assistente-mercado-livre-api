95c5648aa1f489fd8b8ce23e0e0e8e0a8ed9c0a3
Nichollas Falcão <nychollas2013@hotmail.com>
2020-07-01 22:43:58 -0300

    Kickstart

---
+++ src/main/java/br/com/assistentemercadolivre/api/AssistenteMercadoLivreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.assistentemercadolivre.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteMercadoLivreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteMercadoLivreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/assistentemercadolivre/api/AssistenteMercadoLivreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.assistentemercadolivre.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteMercadoLivreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

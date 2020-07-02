40073e1b232cd416bc12a2884605afa2400fff6d
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-07-02 02:42:56 -0300

    handler exception / controller

---
+++ src/main/java/br/com/assistentemercadolivre/api/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package br.com.assistentemercadolivre.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.assistentemercadolivre.api.service.AuthenticationService;
+
+@RestController
+@RequestMapping("api/")
+public class AuthenticationController {
+
+	@Autowired
+	private AuthenticationService service;
+
+	@PostMapping("assistente/auth")
+	public Object authentication(@RequestParam(value = "client_id") String clientId,
+			@RequestParam(value = "client_secret") String clientSecret, @RequestParam(value = "code") String code,
+			@RequestParam(value = "grant_type") String grantType,
+			@RequestParam(value = "redirect_uri") String redirectUri) {
+		return ResponseEntity.ok(service.authentication(clientId, clientSecret, code, grantType, redirectUri));
+	}
+
+}
---
+++ src/main/java/br/com/assistentemercadolivre/api/controller/handler/HandlerException.java
@@ -0,0 +1,22 @@
+package br.com.assistentemercadolivre.api.controller.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.assistentemercadolivre.api.model.ResponseError;
+
+@ControllerAdvice
+public class HandlerException extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(HttpClientErrorException.class)
+	public @ResponseBody ResponseEntity<ResponseError> handleInvalidParameterException(HttpClientErrorException ex) {
+		ResponseError exceptionError = new ResponseError(ex.getMessage(), ex.getStatusCode().value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionError);
+	}
+
+}

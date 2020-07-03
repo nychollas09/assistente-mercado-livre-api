49bcf768d35d307b18743ac036cf24f6042bdaf8
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-07-03 01:18:56 -0300

    api

---
+++ src/main/java/br/com/assistentemercadolivre/api/controller/CategoriesController.java
@@ -0,0 +1,22 @@
+package br.com.assistentemercadolivre.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.assistentemercadolivre.api.service.product.CategoriesService;
+
+@RestController
+@RequestMapping("api/")
+public class CategoriesController {
+
+  @Autowired
+  private CategoriesService service;
+  
+  @GetMapping("find")
+  public ResponseEntity<Object> findAll() {
+    return ResponseEntity.ok(service.findAll());
+  }
+  
+}

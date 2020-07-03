6d1f1c8a4a235210b0c327b181ee4be83706d386
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-07-02 21:34:15 -0300

    melhoria na forma de montar a uri

--- src/main/java/br/com/assistentemercadolivre/api/service/request/RequestComponent.java
+++ src/main/java/br/com/assistentemercadolivre/api/service/request/RequestComponent.java
@@ -1,7 +1,5 @@
 package br.com.assistentemercadolivre.api.service.request;
 
-import java.net.URI;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
@@ -10,8 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class RequestComponent {
 
-	public Object send(RestTemplate restTemplate, URI uri, HttpEntity<Object> entity) {
-		return restTemplate.exchange(uri, HttpMethod.POST, entity, Object.class).getBody();
-	}
+  public Object send(RestTemplate restTemplate, String uri, HttpEntity<Object> entity) {
+    return restTemplate.exchange(uri, HttpMethod.POST, entity, Object.class).getBody();
+  }
 
 }

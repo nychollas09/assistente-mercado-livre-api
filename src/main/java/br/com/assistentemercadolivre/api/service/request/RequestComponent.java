97f7c7e328f7a574c85e954fccbf3783f5e02daf
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-07-03 00:28:01 -0300

    deixando o requestComponent mais generico

--- src/main/java/br/com/assistentemercadolivre/api/service/request/RequestComponent.java
+++ src/main/java/br/com/assistentemercadolivre/api/service/request/RequestComponent.java
@@ -1,15 +1,24 @@
 package br.com.assistentemercadolivre.api.service.request;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RequestComponent {
 
-  public Object send(RestTemplate restTemplate, String uri, HttpEntity<Object> entity) {
-    return restTemplate.exchange(uri, HttpMethod.POST, entity, Object.class).getBody();
+  public Object send(RestTemplate restTemplate, String uri, HttpMethod httpMethod) {
+    HttpEntity<Object> entity = new HttpEntity<>(createHttpHeaders());
+    return restTemplate.exchange(uri, httpMethod, entity, Object.class).getBody();
+  }
+
+  private HttpHeaders createHttpHeaders() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    return headers;
   }
 
 }
